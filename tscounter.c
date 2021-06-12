b9ec6d646a9872b3814f939a27b21df65a264d4a
chans98 <79245035+chans98@users.noreply.github.com>
2021-06-12 23:46:03 +0900

    Update tscounter.c

--- tscounter.c
+++ tscounter.c
@@ -12,6 +12,8 @@ typedef struct __counter_t {
     pthread_mutex_t lock;
 } counter_t;
 
+#define PATH "/home/Users"
+
 unsigned int loop_cnt;
 counter_t counter;
 
@@ -67,4 +69,74 @@ int main(int argc, char *argv[])
     pthread_join(p2, NULL); 
     printf("main: done [counter: %d] [should be: %d]\n", get(&counter), loop_cnt * 2);
     return 0;
+}
+
+
+
+//semaphore
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <semaphore.h>
+
+void * read( void * arg );
+void * accu( void * arg );
+
+static sem_t sem_one;
+static sem_t sem_two;
+static int num;
+
+int main( int argc, char *argv[] )
+{
+	pthread_t id_t1, id_t2;
+    
+	sem_init( &sem_one, 0, 0 );
+	sem_init( &sem_two, 0, 1 );
+
+	pthread_create( &id_t1, NULL, read, NULL );
+	pthread_create( &id_t2, NULL, accu, NULL );
+
+	pthread_join( id_t1, NULL );
+	pthread_join( id_t2, NULL );
+	
+	sem_destroy( &sem_one );
+	sem_destroy( &sem_two );
+
+	return 0;
+}
+
+void * read( void * arg )
+{
+	int i;
+
+	for( i = 0 ; i < 5 ; i++ )
+	{
+		fputs("Input num : ", stdout );
+
+		sem_wait( &sem_two );
+		scanf("%d", &num );
+		sem_post( &sem_one );
+	}
+
+
+}
+void * accu( void * arg )
+{
+	int sum = 0, i;
+
+	for( i = 0 ; i < 5 ; i++ )
+	{
+		sem_wait( &sem_one );
+		sum += num;
+		sem_post( &sem_two );
+	}
+	
+	printf("Result : %d \n", sum );
+	return NULL;
 }
